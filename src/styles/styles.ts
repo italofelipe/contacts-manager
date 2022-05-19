0ffa7bcb0d61eaf469bb6f84b609a01536b72b66
italofelipe <felipe.italo@hotmail.com>
2022-05-19 16:58:29 -0300

    style: added many new styles and new images

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+type Align = "center" | "left" | "right";
 const FormWrapper = styled.form`
   display: flex;
 `;
@@ -10,38 +11,50 @@ const AddButton = styled.button`
   background: #64b5f6;
   color: #fafafa;
   max-width: 150px;
+  border-radius: 4px;
+  border: none;
+  &:hover {
+    cursor: pointer;
+    background: #1e88e5;
+    box-shadow: 1px 2px 4px 1px #9e9e9e;
+  }
 `;
 
 const Wrapper = styled.div`
   display: flex;
-  flex-flow: column;
   width: 100%;
   height: 100%;
-  margin: 2em;
 `;
 
 const AddContact = styled.section`
   max-width: 70vw;
   height: 100%;
   margin: 0 3em;
-`
+`;
 
 const Container = styled.div`
   display: flex;
   flex-flow: row;
-  
+  width: 100%;
 `;
 
-const Box = styled.div`
+type IBox = {
+  w: "full" | "part";
+};
+const Box = styled.div<IBox>`
   display: flex;
   flex-flow: column;
   margin: 15px 10px;
-  box-shadow: 3px 3px 3px 15px #dedede;
+  box-shadow: 0px 0px 12px 3px #dedede;
+  align-items: center;
+  justify-content: space-between;
+  width: ${({ w }) => (w === "full" ? "100%" : "70%")};
 `;
 
 type IText = {
   size: "sm" | "md" | "lg" | "xl";
-  variant: "text" | "caption" | "info" | "warning";
+  variant: "text" | "danger" | "info" | "warning";
+  align: Align;
 };
 const Text = styled.p<IText>`
   font-size: ${(props) => {
@@ -56,6 +69,155 @@ const Text = styled.p<IText>`
         return "2.4em";
     }
   }};
+  text-align: ${(props) => props.align};
+  margin: 0.5em 0 0 0.2em;
+  color: ${({ variant }) => {
+    switch (variant) {
+      case "text":
+        return "#212121";
+      case "danger":
+        return "#EF5350";
+      case "warning":
+        return "#FFA726";
+      default:
+        return "#9E9E9E";
+    }
+  }};
 `;
-export { AddButton, AddContact, Box, Container, FormWrapper, Text, Wrapper };
+
+const Aside = styled.aside`
+  height: 100vh;
+  display: flex;
+  width: 30vw;
+  flex-flow: column;
+`;
+
+type ITitle = {
+  align: Align;
+};
+
+const H2Title = styled.h2<ITitle>`
+  text-align: ${({ align }) => align};
+  font-size: 1.6em;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+`;
+const Contact = styled.div`
+  display: flex;
+  flex-flow: row;
+  &:hover {
+    cursor: pointer;
+    background-color: #81c784;
+  }
+`;
+
+const ContactLeft = styled.div`
+  width: 20%;
+  display: flex;
+  padding-left: 0.3em;
+  flex-flow: column;
+  justify-content: center;
+`;
+const ContactCenter = styled.div`
+  width: 60%;
+  display: flex;
+  flex-flow: column;
+  justify-content: left;
+  padding: 0.25em;
+`;
+const ContactRight = styled.div`
+  width: 20%;
+  display: flex;
+  flex-flow: row;
+
+  align-items: center;
+  justify-content: end;
+
+  svg {
+    max-width: 10px;
+
+    margin-top: 1.2em;
+  }
+`;
+
+const ContactPhotoContainer = styled.div`
+  width: 2em;
+  border-radius: 50%;
+  height: 2em;
+  border: 2px solid red;
+`;
+
+const ContactCard = styled.div`
+  width: 70%;
+  height: 70%;
+  display: flex;
+  flex-flow: column;
+`;
+const ContactCardUpper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row;
+  justify-content: flex-end;
+  svg {
+    width: 3em;
+    height: 3em;
+  }
+`;
+
+const ContactCardInnerContent = styled.div`
+  display: flex;
+  flex-flow: column;
+`;
+const ContactCardLower = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: end;
+  div {
+    background: #ef5350;
+    width: 5em;
+    height: 5em;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1.7em;
+    &:hover {
+      cursor: pointer;
+      box-shadow: 1px 2px 4px 1px #9e9e9e;
+    }
+  }
+  svg {
+    width: 3em;
+    height: 3em;
+  }
+`;
+
+export {
+  AddButton,
+  AddContact,
+  Aside,
+  Box,
+  Contact,
+  ContactCard,
+  ContactCardInnerContent,
+  ContactCardLower,
+  ContactCardUpper,
+  ContactLeft,
+  ContactCenter,
+  ContactRight,
+  ContactPhotoContainer,
+  Container,
+  FormWrapper,
+  H2Title,
+  Text,
+  TextContainer,
+  Wrapper,
+};
 
