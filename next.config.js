68aa23118fa8dacd34e9448e1c83c4c1edd8586e
italofelipe <felipe.italo@hotmail.com>
2022-05-21 01:32:52 -0300

    chore: removed firebase over mirage due to issues with testing and response structure

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_APP_FIREBASE_API_KEY: process.env.NEXT_APP_FIREBASE_API_KEY,
-    NEXT_APP_FIREBASE_AUTH_DOMAIN:
-      process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
-    NEXT_APP_FIREBASE_DATABASE_URL:
-      process.env.NEXT_APP_FIREBASE_DATABASE_URL,
-    NEXT_APP_FIREBASE_PROJECT_ID:
-      process.env.NEXT_APP_FIREBASE_PROJECT_ID,
-    NEXT_APP_FIREBASE_STORAGE_BUCKET:
-      process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
-    NEXT_APP_FIREBASE_MESSAGING_SENDER_ID:
-      process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_APP_FIREBASE_APP_ID: process.env.NEXT_APP_FIREBASE_APP_ID,
-    NEXT_APP_FIREBASE_MEASUREMENT_ID:
-      process.env.NEXT_APP_FIREBASE_MEASUREMENT_ID,
-  },
 };
 
 module.exports = nextConfig;
